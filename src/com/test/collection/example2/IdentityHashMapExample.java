68e91a589f2fca9bc136322e44aad2c862ada42f
Govindaraj <govindaraj_s04@infosys.com>
2017-10-25 08:10:19 +0530

    Test Message

--- src/com/test/collection/example2/IdentityHashMapExample.java
+++ src/com/test/collection/example2/IdentityHashMapExample.java
@@ -4,23 +4,23 @@ import java.util.IdentityHashMap;
 
 public class IdentityHashMapExample {
 
-	public static void main(String[] args) {
-		IdentityHashMap<Integer, String> identity = new IdentityHashMap<Integer,String>();
-		Integer i1 = new Integer(10);
-		Integer i2 = new Integer(10);
-		identity.put(i1, "Govind");
-		identity.put(i2, "Raj");
-		/*
-		 * In the identity map, the key is object hashcode. Although the key is printed as 
-		 * 10, the actual key value will be hashcode. If we try to do identity.get(10) it 
-		 * will return null  
-		 */
-		System.out.println(identity);
-		String s = identity.get(10); 
-		System.out.println(s); //null
-		System.out.println(identity.get(i1));
-		Integer i3 = i1;
-		System.out.println(identity.get(i3));
-	}
+    public static void main(String[] args) {
+        IdentityHashMap<Integer, String> identity = new IdentityHashMap<Integer,String>();
+        Integer i1 = new Integer(10);
+        Integer i2 = new Integer(10);
+        identity.put(i1, "Govind");
+        identity.put(i2, "Raj");
+        /*
+         * In the identity map, the key is object hashcode. Although the key is printed as 
+         * 10, the actual key value will be hashcode. If we try to do identity.get(10) it 
+         * will return null  
+         */
+        System.out.println(identity);
+        String s = identity.get(10); 
+        System.out.println(s); //null
+        System.out.println(identity.get(i1));
+        Integer i3 = i1;
+        System.out.println(identity.get(i3));
+    }
 
 }
--- src/com/test/collection/example2/WeakHashMapExample.java
+++ src/com/test/collection/example2/WeakHashMapExample.java
@@ -4,28 +4,28 @@ import java.util.WeakHashMap;
 
 public class WeakHashMapExample {
 
-	public static void main(String[] args) throws InterruptedException {
-		WeakHashMap<Employee, String> emp = new WeakHashMap<Employee,String>();
-		Employee e = new Employee();
-		emp.put(e, "Raj");
-		System.out.println("***********");
-		System.out.println(emp);
-		System.out.println("###########");
-		e = null;
-		System.gc();
-		Thread.sleep(1000);
-		System.out.println(emp);
-	}
+    public static void main(String[] args) throws InterruptedException {
+        WeakHashMap<Employee, String> emp = new WeakHashMap<Employee,String>();
+        Employee e = new Employee();
+        emp.put(e, "Raj");
+        System.out.println("***********");
+        System.out.println(emp);
+        System.out.println("###########");
+        e = null;
+        System.gc();
+        Thread.sleep(1000);
+        System.out.println(emp);
+    }
 }
 
 class Employee{
-	String name;
-	int id;
-	public String toString(){
-		System.out.println("I am called when printing the map in console");
-		return "temp";
-	}
-	public void finalize(){
-		System.out.println("Calling Finalize method");
-	}
+    String name;
+    int id;
+    public String toString(){
+        System.out.println("I am called when printing the map in console");
+        return "temp";
+    }
+    public void finalize(){
+        System.out.println("Calling Finalize method");
+    }
 }
